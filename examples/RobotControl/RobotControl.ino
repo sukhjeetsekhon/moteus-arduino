9fbd0eb10437a5e0b2d01ffe0868ec8a8a9225aa
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-06-07 21:26:05 -0700

    feat: write example for directional velocity contro of all motorsl for a given angle

--- examples/RobotControl/RobotControl.ino
+++ examples/RobotControl/RobotControl.ino
@@ -1,9 +1,12 @@
 #include <MoteusAcan2517fd.h>
 
 constexpr short VELOCITY = 1;
+constexpr float ANGLE_DEGREES = -70;
+constexpr float ANGLE = ANGLE_DEGREES * PI / 180; // in radians
 
 constexpr short TEST_NUM_MOTORS = 2;
 constexpr short NUM_MOTORS = 5;
+constexpr short NUM_WHEELS = 4;
 
 // MCP2517 pins for CAN FD Arduino Shield
 constexpr byte MCP2517_SCK = 13;  // SCK
@@ -16,7 +19,33 @@ constexpr long CANFD_BITRATE = 1000ll * 1000ll;  // 1 MBit bitrate for CANFD
 
 ACAN2517FD can(MCP2517_CS, SPI, MCP2517_INT);
 
+/**
+   @note motor indices 0-3 are wheel motors
+   @note motor index 4 is the dribbler motor
+*/
 const Moteus* motors[NUM_MOTORS] = {nullptr};
+
+/**
+   @brief angle of wheel relative to the front to back axis in radians
+   @note order: FrontLeft, FrontRight, BackRight, FrontLeft
+   @note front angle is 20 degrees and back angle is 60 degrees
+*/
+constexpr float angles[NUM_WHEELS] = {
+   0.349066,
+   -0.349066,
+   1.0472,
+   -1.0472,
+};
+
+/**
+   @brief should the velocity of the motor be inverted rotation
+*/
+constexpr bool invert[NUM_WHEELS] = {
+   true,
+   false,
+   false,
+   true
+};
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -43,26 +72,27 @@ void setup() {
     delay(1000);
   }
 
-   for (int i=1;i<=TEST_NUM_MOTORS;i++) {
+   for (int i=0;i<TEST_NUM_MOTORS;i++) {
       motors[i] = new Moteus(can, [i]() {
          Moteus::Options options;
-         options.id = i;
+         options.id = i+1;
          return options;
       }());
       // Clear any faults
       motors[i]->SetStop();
    }
-
-  Serial.println(F("motor 1 ready"));
 }
 
 void loop() {
 
   Moteus::PositionMode::Command cmd;
   cmd.position = NaN;  // Pure velocity mode.
-  cmd.velocity = VELOCITY;
 
-   for(int i=1;i<=TEST_NUM_MOTORS;i++) {
+   for(int i=0;i<TEST_NUM_MOTORS;i++) {
+      cmd.velocity = VELOCITY * cos(angles[i] - ANGLE);
+      if (invert[i]) {
+         cmd.velocity *= -1;
+      }
       motors[i]->SetPosition(cmd);
    }
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
